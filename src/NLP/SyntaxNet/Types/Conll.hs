510eb8dd24aa7bf97f7eaedaa9d8758e1d5ad310
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2017-07-10 14:55:59 +0200

    update structure

--- src/NLP/SyntaxNet/Types/Conll.hs
+++ src/NLP/SyntaxNet/Types/Conll.hs
@@ -5,21 +5,22 @@ import Data.Default
 
 --------------------------------------------------------------------------------
 
--- | Row entry containing all relevant information
+-- | Row entry containing row structure for CONLL
+--   all relevant description can be fined at
+--   https://github.com/tensorflow/models/blob/7d30a017fe50b648be6dee544f8059bde52db562/syntaxnet/syntaxnet/text_formats.cc#L62
 data CnllEntry =
   CnllEntry
     { cnId        :: Text -- ^ Index number
-    , cnWord      :: Text -- ^ Parsed word
-    , cnParam     :: Text -- ^ TODO: identify
-    , cnPosCr     :: Text -- ^ Part-of-Speech (POS) coarse-grained (PRON, VERB, DET, NOUN, etc) 
-    , cnPosFn     :: Text -- ^ Part-of-Speech (POS) fine-grained   (PRP, VBD, DT, NN etc.) 
-    , cnParm2     :: Text -- ^ TODO: identify
-    , cnShift     :: Int  -- ^
-    , cnRel       :: Text -- ^ grammatical relationships between different words in the sentence
-    , cnParam3    :: Text -- ^ TODO: identify
-    , cnParam4    :: Text -- ^ TODO: identify     
+    , cnWord      :: Text -- ^ Parsed word or punctuation symbol
+    , cnLemma     :: Text -- ^ Lemma or stem
+    , cnPosCp     :: Text -- ^ Part-of-Speech (POS) coarse-grained (PRON, VERB, DET, NOUN, etc) 
+    , cnPosTag    :: Text -- ^ Part-of-Speech (POS) fine-grained   (PRP, VBD, DT, NN etc.) 
+    , cnFeats     :: Text -- ^ Unordered set of syntactic and/or morphological features.
+    , cnHead      :: Int  -- ^ Head of the current token, which is either a value of ID or '0'.
+    , cnRel       :: Text -- ^ grammatical relationships between different words in the sentence, alined with Head
+    , cnHeadProj  :: Text -- ^ Projective head of current token.
+    , cnRelProj   :: Text -- ^ Dependency relation to the PHEAD.     
     } deriving (Show, Eq)
 
-
 -- TODO: 1. Add Cassava based reader 
---  
+--       2. 
