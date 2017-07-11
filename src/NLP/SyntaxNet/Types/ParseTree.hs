2449485f93d1375708bbef8af2f5f307304b4000
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2017-07-11 11:20:47 +0200

    move to stardard Rose trees

--- src/NLP/SyntaxNet/Types/ParseTree.hs
+++ src/NLP/SyntaxNet/Types/ParseTree.hs
@@ -4,9 +4,11 @@
 module NLP.SyntaxNet.Types.ParseTree where
 
 import Data.Text
+import Data.Tree
 import Data.Default
 
 --------------------------------------------------------------------------------
+
 
 -- | Single Node, containing parse word, its tag and label
 data TreeNode =
@@ -16,13 +18,13 @@ data TreeNode =
     , tnLabel  :: Text -- ^ parsed word assigned Label
     } deriving (Eq, Show)
 
--- | Milti-way tree
-data ParseTree =
-  ParseTree
-    { ptRoot   :: TreeNode    -- ^ labeled value
-    , ptForest :: [ParseTree] -- ^ zero or more child trees
-    } deriving (Eq, Show)
+-- -- | Milti-way tree
+-- data ParseTree =
+--   ParseTree
+--     { ptRoot   :: TreeNode    -- ^ labeled value
+--     , ptForest :: [ParseTree] -- ^ zero or more child trees
+--     } deriving (Eq, Show)
 
   
--- TODO: 1) Add Read instance, to read from SyntaxNet output
---       2) Add Show instance, for nice output 
+-- -- TODO: 1) Add Read instance, to read from SyntaxNet output
+-- --       2) Add Show instance, for nice output 
