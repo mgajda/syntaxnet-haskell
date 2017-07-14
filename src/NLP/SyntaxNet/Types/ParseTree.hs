4a87f2a9771695223179f368a6f58acec9062d5b
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2017-07-14 22:42:44 +0200

    add useful Tree functions

--- src/NLP/SyntaxNet/Types/ParseTree.hs
+++ src/NLP/SyntaxNet/Types/ParseTree.hs
@@ -3,22 +3,50 @@
 
 module NLP.SyntaxNet.Types.ParseTree where
 
-import Data.Text
-import Control.Lens
-import Data.Tree
-import Data.Tree.Lens
-import Data.Default
+import           Control.Lens
+import           Data.Default
+import           Data.Text
+import           Data.Tree
+import           Data.Tree.Lens
+import qualified Data.Map as M
 
 import NLP.SyntaxNet.Types.CoNLL
 
 --------------------------------------------------------------------------------
 
+-- |A 'Tree' of 'Token's
+type TokenTree = Tree Token
+
+-- |A 'Forest' of 'Token's
+type TokenForest = Forest Token
+
+-- |A Map of text values the appropriate tree
+type TokenMap = M.Map Text TokenTree
+
+--------------------------------------------------------------------------------
+
+-- |Given a list of 'TokenTree's, return the top-level 'token's.
+forestTokens :: [TokenTree] -> [Text]
+forestTokens = Prelude.map forestToken
+
+-- |Given a 'TokenTree', return the top-level 'token'.
+forestToken :: TokenTree -> Text
+forestToken (Node tkn subf) = tnWord tkn
+
+-- |Given a list of 'TokenTree's, return a map of each token with
+-- the appropriate tree.
+mkMap :: TokenForest -> TokenMap
+mkMap =
+  M.fromList . tokenTreeAlist
+    where
+      tokenTreeAlist frs = Prelude.zip (forestTokens frs) frs
+
 -- | Convert list of nodes with defined level
 --   into Tree structure
-fromList :: [Token] -> Maybe (Tree Token)
+fromList :: [Token] -> Maybe TokenTree
 fromList nodes =
   Just $ fromListAux nodes 0
-    where fromListAux :: [Token] -> Int -> (Tree Token)
+    where fromListAux :: [Token] -> Int -> TokenTree
           fromListAux = undefined
           
 -- | Convert Tree structure to a sequantial list structure
