61b07d6fbf5714158099c4ffbf3e13acdcec5d47
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2017-07-14 16:29:49 +0200

    update list of fine-grained types

--- src/NLP/SyntaxNet/Types/Conll.hs
+++ src/NLP/SyntaxNet/Types/Conll.hs
@@ -52,10 +52,42 @@ data PosCg =
 
 -- | Sum type for fine-grained part-of-speech
 data PosFg = 
-    PRP
-  | VBD
-  | DT
-  | NN
+    CC	 -- Coordinating conjunction
+  | CD	 -- Cardinal number
+  | DT	 -- Determiner
+  | EX	 -- Existential there
+  | FW	 -- Foreign word
+  | IN	 -- Preposition or subordinating conjunction
+  | JJ	 -- Adjective
+  | JJR	 -- Adjective, comparative
+  | JJS	 -- Adjective, superlative
+  | LS	 -- List item marker
+  | MD	 -- Modal
+  | NN	 -- Noun, singular or mass
+  | NNS	 -- Noun, plural
+  | NNP	 -- Proper noun, singular
+  | NNPS -- Proper noun, plural
+  | PDT  -- Predeterminer
+  | POS  -- Possessive ending
+  | PRP  -- Personal pronoun
+  | PRPS -- Possessive pronoun PRP$
+  | RB   -- Adverb
+  | RBR  -- Adverb, comparative
+  | RBS  -- Adverb, superlative
+  | RP   -- Particle
+  | SYM  -- Symbol
+  | TO   -- to
+  | UH   -- Interjection
+  | VB   -- Verb, base form
+  | VBD  -- Verb, past tense
+  | VBG  -- Verb, gerund or present participle
+  | VBN  -- Verb, past participle
+  | VBP  -- Verb, non-3rd person singular present
+  | VBZ  -- Verb, 3rd person singular present
+  | WDT  -- Wh-determiner
+  | WP   -- Wh-pronoun
+  | WPS  -- Possessive wh-pronoun WP$
+  | WRB  -- Wh-adverb  
   deriving (Show, Eq, Generic)
 
 instance Csv.FromRecord CnllEntry where 
