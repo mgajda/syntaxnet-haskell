6acc38b80d61d03c92e964898b212e0baccf1d2e
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2017-07-16 00:23:15 +0200

    add corrections for algo and debug function

--- src/NLP/SyntaxNet/Types/ParseTree.hs
+++ src/NLP/SyntaxNet/Types/ParseTree.hs
@@ -34,11 +34,11 @@ drawForest'  = P.unlines . P.map drawTree'
 
 draw :: TokenTree -> [String]
 draw (Node tkn ts0) =
-  P.lines (unpack $ tnWord tkn) ++ drawSubTrees ts0
+  P.lines ((unpack $ tnWord tkn) ++ " " ++ (show $ tnPosFG tkn) ++ " " ++ (showPP $ tnRel tkn)) ++ drawSubTrees ts0
     where
       drawSubTrees []     = []
-      drawSubTrees [t]    = " |" : shift " +-- " "   " (draw t)
-      drawSubTrees (t:ts) = " |" : shift " +-- " " |  " (draw t) ++ drawSubTrees ts
+      drawSubTrees [t]    = shift " +-- " "   " (draw t)
+      drawSubTrees (t:ts) = shift " +-- " " |  " (draw t) ++ drawSubTrees ts
 
       shift first other = P.zipWith (++) (first : repeat other)
 
@@ -66,28 +66,29 @@ getLevel :: Forest a -> Int -> [a]
 getLevel fs 0 = P.map rootLabel fs
 getLevel fs n = P.concatMap (\fs' -> getLevel (subForest fs') (n-1)) fs
 
-
 -- | Convert list of nodes with defined level
 --   into Tree structure
 fromList :: [Token] -> Maybe TokenTree
 fromList (n:nodes) =
-  Just $ Node n $ fromListAux nodes [] 
+  Just $ Node n (fromListAux nodes []) 
     where fromListAux :: [Token]      -- ^ List of parsed Tokens
                       -> [Tree Token] -- ^ Building Forest
                       -> [Tree Token] -- ^ Final Forest
           fromListAux []         f = f
           fromListAux (t:ts:tss) f
             -- check current and next level
-            | tnId t == tnId ts      = 
-              -- next element on the same level, attach only
-              f ++ [Node t []]
-            | tnId t <  tnId ts      =
+            | tnId t == tnId ts      = do
+              -- next element on the same level, attach only              
+              fromListAux (ts:tss) (f ++ [Node t []])
+            | tnId t <  tnId ts      = do
               -- attach and move recursevly deep
-              f ++ [(Node t (fromListAux tss []))]
-            | tnId t >  tnId ts      =
-              -- next level is higher, attach only and move forest up
+              fromListAux (ts:tss) (f ++ [(Node t (fromListAux (ts:tss) [] ))])
+            | tnId t >  tnId ts      = do
+              -- next level is higher, attach only and move forest up              
               f ++ [Node t []]
-              
+
+-- | Debug version of fromList inside IO monad
+-- 
 fromList' :: [Token] -> IO (Maybe TokenTree)
 fromList' (n:nodes) = do
   forest <- fromListAux nodes []
@@ -100,16 +101,28 @@ fromList' (n:nodes) = do
             -- check current and next level
             | tnId t == tnId ts      = do
               -- next element on the same level, attach only
-              putStrLn $ "1"
+              let lvl = P.replicate (tnId t) '-'
+                  lvl'= P.replicate (tnId t) ' '
+              putStrLn $ lvl  
+              putStrLn $ lvl' ++ "ch: 1; " ++ "lv: " ++ (show $ tnId t) ++ " ; wr: " ++ (unpack $ tnWord $ t)
+              
               fromListAux (ts:tss) (f ++ [Node t []])
             | tnId t <  tnId ts      = do
               -- attach and move recursevly deep
-              putStrLn "2"
-              sforest <- fromListAux tss []
+              let lvl = P.replicate (tnId t) '-'
+                  lvl'= P.replicate (tnId t) ' '
+              putStrLn $ lvl  
+              putStrLn $ lvl' ++ "ch: 2; " ++ "lv: " ++ (show $ tnId t) ++ " ; wr: " ++ (unpack $ tnWord $ t)
+
+              sforest <- fromListAux (ts:tss) [] -- <
               fromListAux (ts:tss) (f ++ [(Node t sforest)])
             | tnId t >  tnId ts      = do
               -- next level is higher, attach only and move forest up
-              putStrLn $ "3"  
+              let lvl = P.replicate (tnId t) '-'
+                  lvl'= P.replicate (tnId t) ' '
+              putStrLn $ lvl  
+              putStrLn $ lvl' ++ "ch: 2; " ++ "lv: " ++ (show $ tnId t) ++ " ; wr: " ++ (unpack $ tnWord $ t)
+              
               return $ f ++ [Node t []]
 
           
