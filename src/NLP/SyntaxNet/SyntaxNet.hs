9a7a093b2a9ce50f5f355222916965654e760ec5
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2017-07-20 14:02:00 +0300

    change structure to cohere with haskell-conll underlying package

--- src/NLP/SyntaxNet/SyntaxNet.hs
+++ src/NLP/SyntaxNet/SyntaxNet.hs
@@ -15,37 +15,30 @@ import           Control.Monad
 import           Control.Monad.IO.Class
 import           Data.Aeson
 import           Data.Char
+import qualified Data.Csv as Csv
 import           Data.Tree
 import           Data.Maybe
+import           Protolude
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Lazy as BSL
 import           Data.Functor ((<$>))
-import           Data.Csv ( DefaultOrdered (headerOrder)
-                          , FromField (parseField)
-                          , FromNamedRecord (parseNamedRecord)
-                          , Header
-                          , HasHeader(..)
-                          , ToField (toField)
-                          , ToNamedRecord (toNamedRecord)
-                          , (.:)
-                          , (.=)
-                          , DecodeOptions(..)
-                          )
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TEL
-import qualified Data.Csv as Csv
 import qualified Data.Vector as V
 
+import           Data.ConllToken
+import           Data.ParsedSentence
+import           Data.SyntaxTree
+import           Data.TagLabel
 import           NLP.SyntaxNet.Types.CoNLL
-import           NLP.SyntaxNet.Types.ParseTree
 
 --------------------------------------------------------------------------------
 
-readCnll :: FilePath -> IO [Token]
+readCnll :: FilePath -> IO [SnConllToken]
 readCnll fpath = do 
   csvData <- BSL.readFile fpath 
   case TEL.decodeUtf8' csvData of
@@ -53,7 +46,7 @@ readCnll fpath = do
       putStrLn $ "error decoding" ++ (show err)
       return []
     Right dat  -> do
-      let decodingResult = (Csv.decodeWith cnllOptions NoHeader $ TEL.encodeUtf8 dat) :: Either String (V.Vector Token)
+      let decodingResult = (Csv.decodeWith cnllOptions NoHeader $ TEL.encodeUtf8 dat) :: Either String (V.Vector ConllToken)
       case decodingResult of
         Left err  -> do
           putStrLn $ "error decoding" ++ (show err)
@@ -63,7 +56,7 @@ readCnll fpath = do
 
 -- | Reader for Named files with header
 -- 
-readCnll' :: FilePath -> IO [Token]
+readCnll' :: FilePath -> IO [SnConllToken]
 readCnll' fpath = do
   csvData <- BSL.readFile fpath 
   case TEL.decodeUtf8' csvData of
@@ -81,10 +74,10 @@ readCnll' fpath = do
           return $ V.toList vals
 
           
-decodeEntries :: BL.ByteString -> Either String (V.Vector Token)
+decodeEntries :: BL.ByteString -> Either String (V.Vector ConllToken)
 decodeEntries = fmap snd . Csv.decodeByName
 
-decodeEntries' :: BL.ByteString -> Either String (V.Vector Token)
+decodeEntries' :: BL.ByteString -> Either String (V.Vector ConllToken)
 decodeEntries' = fmap snd . Csv.decodeByName
 
 preprocess :: TL.Text -> TL.Text
@@ -107,7 +100,7 @@ cnllOptions =
 --------------------------------------------------------------------------------
 -- Dealing with trees
 
-readParseTree :: FilePath -> IO (Maybe (Tree Token))
+readParseTree :: FilePath -> IO (Maybe (Tree ConllToken))
 readParseTree fpath = do
   treeData <- BSC.readFile fpath
   let ls = BSC.lines treeData
@@ -129,27 +122,28 @@ readParseTree' fpath = do
   mapM_ (putStrLn . show ) ls
 
   let lls  = map ( \x -> BSC.split ' ' x) ls
-      lln  = map parseNode lls
+  
+  lln  <- mapM parseNode' ls 
+    
+  tree <- fromList' $ lln
 
-  tree <- fromList' lln 
-
-  --mapM_ (putStrLn . show ) lln
+  mapM_ (putStrLn . show ) lln
   putStrLn "----\n"
   putStrLn $ drawTree' $ fromJust tree
   
   return $ ()
   
-parseNode :: [BSC.ByteString] -> Token
+parseNode :: [BSC.ByteString] -> ConllToken
 parseNode llbs = do
   let llss  =  map BSC.unpack llbs
       lenLB = 3                             -- useful labels like TOKEN POS ER
-      lenWP = length $ filter (=="  ") llss -- each identation takes 2 spaces
+      lenWP = (length $ filter (=="  ") llss ) -- each identation takes 2 spaces
 
   let lls  = map T.pack llss
       lvl  = (length lls) - lenLB - lenWP   -- calculate actual level  
       lbls = drop ((length lls) - 3) lls    -- extract only lables      
 
-  Token lvl                                 -- reuse id to indicate level, when working with trees          
+  ConllToken lvl                                 -- reuse id to indicate level, when working with trees          
         (lbls!!0)
         ""
         UnkCg
@@ -159,3 +153,32 @@ parseNode llbs = do
         (parseGER $ T.unpack $ lbls!!2)
         ""
         ""
+        
+parseNode' :: BSC.ByteString -> IO ConllToken
+parseNode' bs = do
+  let llbs  = filter (/="|") $ BSC.split ' ' bs
+      llss  = map BSC.unpack llbs
+      lenLB = 3                                                  -- useful labels like TOKEN POS ER
+      lenWP = (length $ filter (=="") llss) -- always have 1 in front,  each identation takes 2 spaces
+
+  
+  let llt  = map T.pack llss
+      llsf = filter (/="") llt
+      lvl  = ((mod lenWP 2))+1   -- calculate actual level  
+      lbls = drop ((length llt) - 3) llt    -- extract only lables
+
+  putStrLn $ show $ llss
+  putStrLn $ show $ llsf
+  putStrLn $ show $ lenWP
+  putStrLn $ show $ lvl
+
+  return $ ConllToken lvl                                 -- reuse id to indicate level, when working with trees          
+                 (lbls!!0)
+                 ""
+                 UnkCg
+                 (parsePosFg $ T.unpack $ lbls!!1)
+                 ""
+                 0
+                 (parseGER $ T.unpack $ lbls!!2)
+                 ""
+                 ""
--- src/NLP/SyntaxNet/Types/CoNLL.hs
+++ src/NLP/SyntaxNet/Types/CoNLL.hs
@@ -4,35 +4,27 @@
 
 module NLP.SyntaxNet.Types.CoNLL where
 
-import           Data.Char (toUpper, isUpper, toLower)
-import qualified Data.ByteString       as BS
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
-import qualified Data.Text             as T
-import           Data.List
+import           Data.Char (toUpper, isUpper, toLower)
+import           Data.ConllToken (ConllToken(..), SyntaxErrorCoNLL(..))
+import           Data.Csv as Csv
 import           Data.Default
-import           Data.Csv              as Csv
-import           GHC.Generics                          
+--import           Data.List
+import           Protolude
+import           Data.List.Split
+import           Data.SyntaxTree (SyntaxtTree(..), createSyntaxTree)
+import           Data.TagLabel
+import qualified Data.Text as T
+import           GHC.Generics
+import           Model.PennTreebank
+import           Model.UniversalTreebank
+import           Protolude
                  
 --------------------------------------------------------------------------------
 
--- | Row entry containing row structure for CONLL
---   all relevant description can be fined at
---   https://github.com/tensorflow/models/blob/7d30a017fe50b648be6dee544f8059bde52db562/syntaxnet/syntaxnet/text_formats.cc#L62
---   NB: can be renamed to Token/Entry/Word/  
-data Token =
-  Token
-    { tnId        :: Int     -- ^ Index number
-    , tnWord      :: T.Text  -- ^ Parsed word or punctuation symbol
-    , tnLemma     :: T.Text  -- ^ Lemma or stem
-    , tnPosCG     :: PosCG   -- ^ Part-of-Speech (POS) coarse-grained (PRON, VERB, DET, NOUN, etc) 
-    , tnPosFG     :: PosFG   -- ^ Part-of-Speech (POS) fine-grained   (PRP, VBD, DT, NN etc.) 
-    , tnFeats     :: T.Text  -- ^ Unordered set of syntactic and/or morphological features.
-    , tnHead      :: Int     -- ^ Head of the current token, which is either a value of ID or '0'.
-    , tnRel       :: GER     -- ^ grammatical relationships between different words in the sentence, alined with Head
-    , tnHeadProj  :: T.Text  -- ^ Projective head of current token.
-    , tnRelProj   :: T.Text  -- ^ Dependency relation to the PHEAD.     
-    } deriving (Show, Eq, Generic)
-
+type SnConllToken a = ConllToken  PosCG POS REL T.Text a
+type SnConllTree  a = SyntaxtTree PosCG POS REL T.Text a
 
 -- | See "A Universal Part-of-Speech Tagset" by Slav Petrov, Dipanjan Das and Ryan McDonald
 --   for more details http://arxiv.org/abs/1104.2086
@@ -51,134 +43,15 @@ data PosCG =
   | X      -- other: foreign words, typos, abbreviations
   | PUNCT  -- punctuation
   | UnkCg  -- unknown
-  deriving (Show, Eq, Generic)
-
--- | Sum type for fine-grained part-of-speech (pos)
-data PosFG = 
-    CC   -- Coordinating conjunction
-  | CD   -- Cardinal number
-  | DT   -- Determiner
-  | EX   -- Existential there
-  | FW   -- Foreign word
-  | IN   -- Preposition or subordinating conjunction
-  | JJ   -- Adjective
-  | JJR  -- Adjective, comparative
-  | JJS  -- Adjective, superlative
-  | LS   -- List item marker
-  | MD   -- Modal
-  | NN   -- Noun, singular or mass
-  | NNS  -- Noun, plural
-  | NNP  -- Proper noun, singular
-  | NNPS -- Proper noun, plural
-  | PDT  -- Predeterminer
-  | POS  -- Possessive ending
-  | PRP  -- Personal pronoun
-  | PRPS -- Possessive pronoun PRP$
-  | RB   -- Adverb
-  | RBR  -- Adverb, comparative
-  | RBS  -- Adverb, superlative
-  | RP   -- Particle
-  | SYM  -- Symbol
-  | TO   -- to
-  | UH   -- Interjection
-  | VB   -- Verb, base form
-  | VBD  -- Verb, past tense
-  | VBG  -- Verb, gerund or present participle
-  | VBN  -- Verb, past participle
-  | VBP  -- Verb, non-3rd person singular present
-  | VBZ  -- Verb, 3rd person singular present
-  | WDT  -- Wh-determiner
-  | WP   -- Wh-pronoun
-  | WPS  -- Possessive wh-pronoun WP$
-  | WRB  -- Wh-adverb
-  | UnkFg
-  deriving (Show, Eq, Generic)
-
--- | Grammatical Entity Relation (GER), also known as dependecies
---   Currently English set http://universaldependencies.org/en/dep/all.html
---   TODO: should be extender
-data GER =
-    Acl
-  | AclRelcl
-  | Advcl
-  | Advmod
-  | Amod
-  | Appos
-  | Aux
-  | Auxpass
-  | Case
-  | Cc
-  | CcPreconj
-  | Ccomp
-  | Compound
-  | CompoundPrt
-  | Conj
-  | Cop
-  | Csubj
-  | Csubjpass
-  | Dep
-  | Det
-  | DetPredet
-  | Discourse
-  | Discolated 
-  | Dobj
-  | Expl
-  | Fixed
-  | FixedNot
-  | Flat
-  | Foreign
-  | Goeswith
-  | Iobj
-  | List
-  | Mark
-  | Neg
-  | Nmod
-  | NmodNpmod
-  | NmodPoss
-  | NmodTmod
-  | Nsubj
-  | NsubjPass
-  | Nummod
-  | Orphan
-  | Parataxis
-  | Punct
-  | Reparandum
-  | Root
-  | Vocative
-  | XComp
-  | UnkGer           -- unknown
-  deriving (Show, Eq, Generic)
-
-showPP :: GER -> String
-showPP v = 
-  case v of
-    UnkGer    -> "unknown"
-    Root      -> "ROOT"
-    otherwise ->
-      map toLower $ intercalate ":" $ splitR isUpper $ show v
-      where
-        splitR :: (Char -> Bool) -> String -> [String]
-        splitR _ [] = []
-        splitR p s =
-          let
-            go :: Char -> String -> [String]
-            go m s' = case break p s' of
-              (b', [])     -> [ m:b' ]
-              (b', (x:xs)) -> ( m:b' ) : go x xs
-          in case break p s of
-            (b,  [])    -> [ b ]
-            ([], (h:t)) -> go h t
-            (b, (h:t))  -> b : go h t
-  
-              
+  deriving (Show, Eq, Generic)              
 
 --------------------------------------------------------------------------------
         
 -- | TODO: Check if SyntaxTree can generate named output
 --   where
-instance Csv.FromNamedRecord Token where
+instance Csv.FromNamedRecord SnConllToken where
   parseNamedRecord m =
-    Token
+    SnConllToken
       <$> m Csv..: "param1"
       <*> m Csv..: "param2"
       <*> m Csv..: "param3"
@@ -190,9 +63,9 @@ instance Csv.FromNamedRecord Token where
       <*> m Csv..: "param9"
       <*> m Csv..: "param10"
 
-instance Csv.FromRecord Token where 
+instance Csv.FromRecord SnConllToken where 
   parseRecord v =
-    Token
+    SnConllToken
       <$> v .! 0
       <*> v .! 1
       <*> v .! 2
@@ -204,117 +77,117 @@ instance Csv.FromRecord Token where
       <*> v .! 8
       <*> v .! 9
 
--- | Converting coarse-grained textual types int
---   ADT representation
-parsePosCf :: String -> PosCG
-parsePosCf s =
-  case (map toUpper s) of
-    "VERB" -> VERB
-    "NOUN" -> NOUN
-    "PRON" -> PRON
-    "ADJ"  -> ADJ
-    "ADV"  -> ADV
-    "ADP"  -> ADP    
-    "CONJ" -> CONJ   
-    "DET"  -> DET    
-    "NUM"  -> NUM    
-    "PRT"  -> PRT    
-    "X"    -> X      
-    "."    -> PUNCT      
-    otherwise -> UnkCg
+-- -- | Converting coarse-grained textual types int
+-- --   ADT representation
+-- parsePosCf :: String -> PosCG
+-- parsePosCf s =
+--   case (map toUpper s) of
+--     "VERB" -> VERB
+--     "NOUN" -> NOUN
+--     "PRON" -> PRON
+--     "ADJ"  -> ADJ
+--     "ADV"  -> ADV
+--     "ADP"  -> ADP    
+--     "CONJ" -> CONJ   
+--     "DET"  -> DET    
+--     "NUM"  -> NUM    
+--     "PRT"  -> PRT    
+--     "X"    -> X      
+--     "."    -> PUNCT      
+--     otherwise -> UnkCg
     
--- | Converting fine-grained textual types into
---   ADT representation
-parsePosFg :: String -> PosFG
-parsePosFg s =
-  case (map toUpper s) of
-    "CC"  -> CC  
-    "CD"  -> CD
-    "DT"  -> DT
-    "EX"  -> EX
-    "FW"  -> FW
-    "IN"  -> IN
-    "JJ"  -> JJ
-    "JJR" -> JJR
-    "JJS" -> JJS
-    "LS"  -> LS
-    "MD"  -> MD
-    "NN"  -> NN
-    "NNS" -> NNS
-    "NNP" -> NNP
-    "NNPS"-> NNPS
-    "PDT" -> PDT
-    "POS" -> POS
-    "PRP" -> PRP
-    "PRPS"-> PRPS
-    "RB"  -> RB
-    "RBR" -> RBR
-    "RBS" -> RBS
-    "RP"  -> RP
-    "SYM" -> SYM
-    "TO"  -> TO
-    "UH"  -> UH
-    "VB"  -> VB
-    "VBD" -> VBD
-    "VBG" -> VBG
-    "VBN" -> VBN
-    "VBP" -> VBP
-    "VBZ" -> VBZ
-    "WDT" -> WDT
-    "WP"  -> WP
-    "WPS" -> WPS
-    "WRB" -> WRB
-    otherwise -> UnkFg
+-- -- | Converting fine-grained textual types into
+-- --   ADT representation
+-- parsePosFg :: String -> PosFG
+-- parsePosFg s =
+--   case (map toUpper s) of
+--     "CC"  -> CC  
+--     "CD"  -> CD
+--     "DT"  -> DT
+--     "EX"  -> EX
+--     "FW"  -> FW
+--     "IN"  -> IN
+--     "JJ"  -> JJ
+--     "JJR" -> JJR
+--     "JJS" -> JJS
+--     "LS"  -> LS
+--     "MD"  -> MD
+--     "NN"  -> NN
+--     "NNS" -> NNS
+--     "NNP" -> NNP
+--     "NNPS"-> NNPS
+--     "PDT" -> PDT
+--     "POS" -> POS
+--     "PRP" -> PRP
+--     "PRPS"-> PRPS
+--     "RB"  -> RB
+--     "RBR" -> RBR
+--     "RBS" -> RBS
+--     "RP"  -> RP
+--     "SYM" -> SYM
+--     "TO"  -> TO
+--     "UH"  -> UH
+--     "VB"  -> VB
+--     "VBD" -> VBD
+--     "VBG" -> VBG
+--     "VBN" -> VBN
+--     "VBP" -> VBP
+--     "VBZ" -> VBZ
+--     "WDT" -> WDT
+--     "WP"  -> WP
+--     "WPS" -> WPS
+--     "WRB" -> WRB
+--     otherwise -> UnkFg
     
-parseGER :: String -> GER
-parseGER s =
-  case s of
-    "acl"         -> Acl
-    "acl:relcl"   -> AclRelcl
-    "advck"       -> Advcl
-    "advmod"      -> Advmod
-    "amod"        -> Amod
-    "appos"       -> Appos
-    "aux"         -> Aux
-    "auxpass"     -> Auxpass
-    "case"        -> Case
-    "cc"          -> Cc
-    "cc:preconj"  -> CcPreconj
-    "ccomp"       -> Ccomp
-    "compound"    -> Compound
-    "compound:prt"-> CompoundPrt
-    "conj"        -> Conj
-    "cop"         -> Cop
-    "csubj"       -> Csubj
-    "csubjpass"   -> Csubjpass
-    "dep"         -> Dep
-    "det"         -> Det
-    "det:predet"  -> DetPredet
-    "discource"   -> Discourse
-    "discolated"  -> Discolated 
-    "dobj"        -> Dobj
-    "expl"        -> Expl
-    "fixed"       -> Fixed
-    "fixed:not"   -> FixedNot
-    "flat"        -> Flat
-    "foreign"     -> Foreign
-    "goeswith"    -> Goeswith
-    "iobj"        -> Iobj
-    "list"        -> List
-    "mark"        -> Mark
-    "neg"         -> Neg
-    "nmod"        -> Nmod
-    "nmod:npmod"  -> NmodNpmod
-    "nmod:poss"   -> NmodPoss
-    "nmod:tmod"   -> NmodTmod
-    "nsubj"       -> Nsubj
-    "nsubjpass"   -> NsubjPass
-    "nummod"      -> Nummod
-    "orphan"      -> Orphan
-    "parataxis"   -> Parataxis
-    "punct"       -> Punct
-    "reparandum"  -> Reparandum
-    "ROOT"        -> Root
-    "vocatile"    -> Vocative
-    "xcomp"       -> XComp
-    otherwise     -> UnkGer
+-- parseGER :: String -> GER
+-- parseGER s =
+--   case s of
+--     "acl"         -> Acl
+--     "acl:relcl"   -> AclRelcl
+--     "advck"       -> Advcl
+--     "advmod"      -> Advmod
+--     "amod"        -> Amod
+--     "appos"       -> Appos
+--     "aux"         -> Aux
+--     "auxpass"     -> Auxpass
+--     "case"        -> Case
+--     "cc"          -> Cc
+--     "cc:preconj"  -> CcPreconj
+--     "ccomp"       -> Ccomp
+--     "compound"    -> Compound
+--     "compound:prt"-> CompoundPrt
+--     "conj"        -> Conj
+--     "cop"         -> Cop
+--     "csubj"       -> Csubj
+--     "csubjpass"   -> Csubjpass
+--     "dep"         -> Dep
+--     "det"         -> Det
+--     "det:predet"  -> DetPredet
+--     "discource"   -> Discourse
+--     "discolated"  -> Discolated 
+--     "dobj"        -> Dobj
+--     "expl"        -> Expl
+--     "fixed"       -> Fixed
+--     "fixed:not"   -> FixedNot
+--     "flat"        -> Flat
+--     "foreign"     -> Foreign
+--     "goeswith"    -> Goeswith
+--     "iobj"        -> Iobj
+--     "list"        -> List
+--     "mark"        -> Mark
+--     "neg"         -> Neg
+--     "nmod"        -> Nmod
+--     "nmod:npmod"  -> NmodNpmod
+--     "nmod:poss"   -> NmodPoss
+--     "nmod:tmod"   -> NmodTmod
+--     "nsubj"       -> Nsubj
+--     "nsubjpass"   -> NsubjPass
+--     "nummod"      -> Nummod
+--     "orphan"      -> Orphan
+--     "parataxis"   -> Parataxis
+--     "punct"       -> Punct
+--     "reparandum"  -> Reparandum
+--     "ROOT"        -> Root
+--     "vocatile"    -> Vocative
+--     "xcomp"       -> XComp
+--     otherwise     -> UnkGer
